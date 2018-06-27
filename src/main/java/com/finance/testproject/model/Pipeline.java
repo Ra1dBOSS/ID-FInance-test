bd57c649750fa4a37c505b2e347351775f22b2b3
Ra1dBOSS <boss97cool@mail.ru>
2018-06-27 18:20:36 +0300

    hibernate support added

--- src/main/java/com/finance/testproject/model/Pipeline.java
+++ src/main/java/com/finance/testproject/model/Pipeline.java
@@ -1,13 +1,23 @@
 package com.finance.testproject.model;
 
+import javax.persistence.*;
 import java.util.List;
 
+@Entity
+@Table(name = "pipelines", schema = "public")
 public class Pipeline {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name="name")
     private String name;
 
+    @Column(name = "description")
     private String description;
 
-    private List<Task> tasks;
+//    @Column
+//    private List<Task> tasks;
 
 }
